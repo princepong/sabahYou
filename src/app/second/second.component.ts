c3d59673621fd0ba30c9c84c1cd05237b0050359
Pong Prince <wongtszpong@gmail.com>
2024-04-28 20:19:49 +0800

    navbar updated

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-second',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './second.component.html',
   styleUrl: './second.component.css'
 })
 export class SecondComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
